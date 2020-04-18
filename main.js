7719a3dcca1ffe66a942a6f6f8f2d3a4da71e237
Maja Jensen <eaamnje@students.eaaa.dk>
2020-04-18 11:12:31 +0200

    Added files

---
+++ main.js
@@ -0,0 +1,26 @@
+// "use strict";
+// Import pages
+
+
+
+
+// Import services
+// import SpaService from "./services/spa.js";
+// import LoaderService from "./services/loader.js";
+// import fetchPersons from "./services/apiConfig.js";
+import appendPersons from "./services/personsPage.js";
+
+
+// // Declare and init pages
+
+
+
+// // Declare and init services
+
+// let spaService = new SpaService("home");
+// let loaderService = new LoaderService();
+// loaderService.show(false);
+
+// window.pageChange = function () {
+//     spaService.pageChange();
+// }
